5ffd872c4a85edf964ac3c5ef98f69a212d4ad41
prismskey <sergios@sunypoly.edu>
2020-05-11 22:12:18 -0400

    Change Event Priority

--- src/main/java/me/prismskey/foreveradorable/foreveradorable/listeners/PlayerInteractListener.java
+++ src/main/java/me/prismskey/foreveradorable/foreveradorable/listeners/PlayerInteractListener.java
@@ -5,6 +5,7 @@ import me.prismskey.foreveradorable.foreveradorable.events.PlayerUseNameTagEvent
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
@@ -15,8 +16,13 @@ public class PlayerInteractListener implements Listener {
 		Bukkit.getServer().getPluginManager().registerEvents(this, ForeverAdorable.instance);
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEntityEvent event) {
+		//just to be safe
+		if(event.isCancelled()) {
+			return;
+		}
+
 		ItemStack heldItem = event.getPlayer().getInventory().getItemInMainHand();
 		if(heldItem.getType() == Material.NAME_TAG) {
 			PlayerUseNameTagEvent nameTagEvent = new PlayerUseNameTagEvent(event.getPlayer(), event.getRightClicked(), heldItem);
